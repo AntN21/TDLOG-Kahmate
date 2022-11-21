c9fdd7183a69c846e065e0e7220a42a67c322e65
Icel <icel.vinals@gmail.com>
2022-11-21 14:26:59 +0100

    Attempt at displaying game from matrix and adding changes with python

--- website/views.py
+++ website/views.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template, request
+import re
+import random
+import numpy as np
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
+chips = [["" for x in range(11)] for x in range(8)]
 
 
-@views.route('/')
-def home():
-    return render_template("home.html")
+@views.route("/", methods=["GET", "POST"])
+def game():
+    print(request.form)
+    if request.method == "POST":
+        position = re.sub(r"[() ]", "", request.form["next"]).split(",")
+        chips[int(position[0])][int(position[1])] = random.choice(
+            ["c", "o", "f", "s", "t", "x"]
+        )
+        if random.randint(0, 2) == 1:
+            chips[int(position[0])][int(position[1])] += "b"
+    return render_template("game.html", chips=chips)
